67427179d8997bb96d740376d67cdfa5b7029c62
Amit0412 <59620280+Amit0412@users.noreply.github.com>
2023-03-27 22:47:49 +0530

    Update File

--- Packet_Header_Extract/packher.py
+++ Packet_Header_Extract/packher.py
@@ -15,11 +15,19 @@ cap=pcapy.open_live(inf,65536,1,0)
 
 while 1:
         (header,payload)=cap.next()
+        
+        #layer header 
         l2hdr=payload[:14]
+        
+        #layer data
         l2data=unpack("!6s6sH",l2hdr)
+        
+        #source mac address
         
         srcmac="%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" %((l2hdr[0]),(l2hdr[1])
         ,(l2hdr[2]),(l2hdr[3]),(l2hdr[4]),(l2hdr[5]))
+        
+        #destination mac address 48 bit.
         
         dstmac="%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" %((l2hdr[6]),(l2hdr[7])
         ,(l2hdr[8]),(l2hdr[9]),(l2hdr[10]),(l2hdr[11]))
